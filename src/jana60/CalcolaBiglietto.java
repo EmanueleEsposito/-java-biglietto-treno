6c2757cbec2995ede690bb55acbe76407d3bde84
EmanueleEsposito <manu2022@blu.it>
2022-06-07 15:00:33 +0200

    aggiunto scanner e calcolo biglietto senza sconto

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -1,10 +1,32 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	int numeroDiKilometri;
+	int etaDelPassegero;
+	double prezzoBigliettoAlKilometro= 0.21;
+	
+	
+	Scanner scan = new Scanner(System.in);
+		System.out.print("Inserisci numero di kilometri che vuoi percorrere: ");
+		numeroDiKilometri = scan.nextInt();
+		
+        System.out.print("Prezzo biglietto senza sconto ");
+        
+double prezzoBigliettoSenzaSconto= numeroDiKilometri*prezzoBigliettoAlKilometro;
+System.out.println(prezzoBigliettoSenzaSconto + "€");
+        prezzoBigliettoSenzaSconto= scan.nextDouble();		
 
+		
+		
+		
+		
+    scan.close();
 	}
 
 }
