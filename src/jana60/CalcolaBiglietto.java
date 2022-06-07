0bfd7359e376b146be33c22ec8aa8eb22014dd86
EmanueleEsposito <manu2022@blu.it>
2022-06-07 15:50:41 +0200

    aggiunti double sconto e if eta del passegero

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -17,14 +17,31 @@ public class CalcolaBiglietto {
 		numeroDiKilometri = scan.nextInt();
 		
         System.out.print("Prezzo biglietto senza sconto ");
-        
-double prezzoBigliettoSenzaSconto= numeroDiKilometri*prezzoBigliettoAlKilometro;
+    	
+        double prezzoBigliettoSenzaSconto= numeroDiKilometri*prezzoBigliettoAlKilometro;
 System.out.println(prezzoBigliettoSenzaSconto + "€");
-        prezzoBigliettoSenzaSconto= scan.nextDouble();		
+        
+System.out.print("Inserisci età del passeggero: ");
+        etaDelPassegero = scan.nextInt();
+//sconto    
+ double prezzoTotaleDelViaggio;
+ double scontoMinorenni= 0.20;
+ double scontoOver65= 0.40;
+ double scontoSulPrezzoMinorenni = prezzoBigliettoSenzaSconto * scontoMinorenni;
+ double scontoSulPrezzoOver65 = prezzoBigliettoSenzaSconto * scontoOver65;
+ double prezzoTotaleViaggioMinorenni = prezzoBigliettoSenzaSconto - scontoMinorenni;
+ double prezzoTotaleViaggioOver65 = prezzoBigliettoSenzaSconto - scontoOver65;
+ 
+if(etaDelPassegero < 18) {
+	System.out.println("Prezzo totale viaggio per minorenne: ");
 
-		
-		
-		
+}else	if(etaDelPassegero > 65)
+	System.out.println("Prezzo totale viaggio per over65: ");
+
+
+ 
+ 
+ 
 		
     scan.close();
 	}
